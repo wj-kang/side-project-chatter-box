e71bef1bf72b812eebcd772afe1d032419bbf13e
Wonjun Kang <michael.wonjun.kang@gmail.com>
2021-05-13 21:20:10 -0500

    Add react-router and fix minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { Switch, Route, Link, useParams } from 'react-router-dom';
 import PageContainer from './components/PageContainer';
 import MainPage from './pages/MainPage';
+import WaitPage from './pages/WaitPage';
 
 function App() {
   return (
     <>
       <PageContainer>
-        <MainPage />
+        <Switch>
+          <Route path="/main" children={<MainPage />} />
+          <Route path="/wait" children={<WaitPage handleJoinChat={() => console.log('asdf')} />} />
+          <Route path="/room/:roomId" children={<></>} />
+          <PageNotFound />
+        </Switch>
       </PageContainer>
     </>
   );
+}
+
+// function Temp() {
+//   const { roomId } = useParams();
+//   return <h3>{roomId}</h3>;
+// }
+
+function PageNotFound() {
+  return <h3>404!</h3>;
 }
 
 export default App;
