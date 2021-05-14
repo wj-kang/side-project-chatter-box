4ad4fa7755e7156101352f704b7f43a420bde915
Wonjun Kang <michael.wonjun.kang@gmail.com>
2021-05-13 22:28:01 -0500

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseDb = firebase.database();
+export const firebaseAuth = firebase.auth();
