120ee381c349489da5c17a70c8c575486cc1995d
Wonjun Kang <michael.wonjun.kang@gmail.com>
2021-05-13 22:28:43 -0500

    Add firebase auth service

---
+++ src/auth_service.js
@@ -0,0 +1,31 @@
+import { firebaseAuth } from './firebase';
+
+class AuthService {
+  guset() {
+    return firebaseAuth.signInAnonymously();
+  }
+
+  logout() {
+    firebaseAuth.signOut();
+  }
+
+  deleteAccount() {
+    const user = firebaseAuth.currentUser;
+    user
+      .delete()
+      .then(function () {
+        console.log('account deleted');
+      })
+      .catch(function (error) {
+        console.log('Error: failed to delete the account');
+      });
+  }
+
+  onAuthChange(context) {
+    firebaseAuth.onAuthStateChanged((user) => {
+      context(user);
+    });
+  }
+}
+
+export default AuthService;
