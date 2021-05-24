826734aa8bae2b7d332b4ca202b29f996f11fcce
Wonjun Kang <michael.wonjun.kang@gmail.com>
2021-05-23 21:19:56 -0500

    Add sidebar menu component for mobile view chat room

---
+++ src/components/chatpage/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import UserList from './UserList';
+import styles from '../../styles/Sidebar.module.css';
+import chatStyles from '../../styles/ChatPage.module.css';
+
+const Sidebar = ({ isSidebarOpen, roomsInfo, nickname, handleLeaveChat, setSidebarOpen }) => {
+  const handleSidebarLeaveBtn = () => {
+    handleLeaveChat();
+  };
+
+  const handleClickBackground = (e) => {
+    if (e.target.id === 'background') {
+      setSidebarOpen(false);
+    }
+  };
+
+  return (
+    <section //
+      id="background"
+      className={`${styles.background} ${isSidebarOpen && styles.on}`}
+      onClick={handleClickBackground}
+    >
+      <div className={styles.sidebar}>
+        <button //
+          className={styles.hamburger}
+          onClick={() => setSidebarOpen(false)}
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <div className={chatStyles.left_nickname_container}>
+          <h3 className={chatStyles.text_h3}>{nickname}</h3>
+        </div>
+
+        <UserList roomsInfo={roomsInfo} />
+
+        <button //
+          className={styles.leave}
+          onClick={handleSidebarLeaveBtn}
+        >
+          Leave
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ChatContainer from '../components/chatpage/ChatContainer';
 import ChatInput from '../components/chatpage/ChatInput';
+import Sidebar from '../components/chatpage/Sidebar';
 import UserList from '../components/chatpage/UserList';
 import styles from '../styles/ChatPage.module.css';
 
 const ChatPage = ({ dbService, uid, roomId, roomsInfo }) => {
   const [popMsg, setPopMsg] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
   const handleNewMsg = (msg) => {
     const nickname = roomsInfo.users[uid].nickname;
@@ -26,8 +28,30 @@ const ChatPage = ({ dbService, uid, roomId, roomsInfo }) => {
   };
 
   const handleLeaveChat = () => {
-    dbService.leaveRoom(roomId, uid, roomsInfo.users[uid].nickname);
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Leave the chat room')) {
+      dbService.leaveRoom(roomId, uid, roomsInfo.users[uid].nickname);
+      setSidebarOpen(false);
+    }
   };
+
+  const leaveBtn = (
+    <button //
+      className={styles.right_top_leave}
+      onClick={handleLeaveChat}
+    >
+      Leave
+    </button>
+  );
+
+  const hamburgerBtn = (
+    <button //
+      className={styles.right_top_hamburger}
+      onClick={() => setSidebarOpen(true)}
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+  );
 
   return (
     <>
@@ -41,18 +65,14 @@ const ChatPage = ({ dbService, uid, roomId, roomsInfo }) => {
         <div className={styles.right_section}>
           <div className={styles.right_top}>
             <div className={styles.right_top_title_container}>
-              <h3 className={styles.right_top_title}>{`Room #${roomId}`}</h3>
+              <h3 className={styles.right_top_title}>{`#${roomId}`}</h3>
               <button className={styles.right_top_linkcopy} onClick={handleCopyLinkBtn}>
                 <i class="fas fa-link"></i>
               </button>
             </div>
-
-            <button //
-              className={styles.right_top_leave}
-              onClick={handleLeaveChat}
-            >
-              Leave
-            </button>
+            {/* Hamburger btn is only for mobile  */}
+            {hamburgerBtn}
+            {leaveBtn}
           </div>
           <ChatContainer //
             dbService={dbService}
@@ -68,6 +88,14 @@ const ChatPage = ({ dbService, uid, roomId, roomsInfo }) => {
         id="link"
         className={styles.hiddenlink}
         value={`${process.env.REACT_APP_BASE_URL}/room/${roomId}`}
+      />
+
+      <Sidebar //
+        isSidebarOpen={isSidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        roomsInfo={roomsInfo}
+        nickname={roomsInfo.users[uid].nickname}
+        handleLeaveChat={handleLeaveChat}
       />
 
       <div className={`${styles.pop_msg} ${popMsg && styles.on}`}>Invite Link Copied</div>
